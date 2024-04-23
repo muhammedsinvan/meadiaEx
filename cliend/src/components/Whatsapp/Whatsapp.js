9e466775abe72de5dc6a46ea302e5383151cf4d0
sinvan <muhammedsinvan3@gmail.com>
2024-04-23 15:42:05 +0530

    number change on whatsapp

--- cliend/src/components/Whatsapp/Whatsapp.js
+++ cliend/src/components/Whatsapp/Whatsapp.js
@@ -6,7 +6,7 @@ import ReactWhatsapp from 'react-whatsapp';
 const Whatsapp = () => {
   return (
     <div className='whatsapp-container'>
-        <ReactWhatsapp className='whatsapp-react' number="+91 8089186853" message="Hello, I have a question about https://www.exmedia.in/">
+        <ReactWhatsapp className='whatsapp-react' number="+91 9656123466" message="Hello, I have a question about https://www.exmedia.in/">
         <WhatsAppIcon  className='whatsapp-icon'/>
         <text>Message Us</text>
         </ReactWhatsapp>
